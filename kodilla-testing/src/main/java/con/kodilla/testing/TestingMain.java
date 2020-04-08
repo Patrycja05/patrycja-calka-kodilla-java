63adb87407a14b4ff27feb01e00ea59e2fa53d20
Patrycja05 <patrycja.calka@wp.pl>
2020-04-08 18:55:15 +0200

    Test

--- kodilla-testing/src/main/java/con/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/con/kodilla/testing/TestingMain.java
@@ -1,7 +1,20 @@
 package con.kodilla.testing;
 
+import con.kodilla.testing.calculator.Calculator;
+
 public class TestingMain {
     public static void main(String[] args) {
         System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+
+        Calculator calculator = new Calculator();
+
+        int sum = calculator.add(5,7);
+        int sub = calculator.subtract(7, 5);
+
+        if(sum == 12 && sub == 2){
+            System.out.println("Great job!");
+        } else {
+            System.out.println("Correct your code!");
+        }
     }
 }
