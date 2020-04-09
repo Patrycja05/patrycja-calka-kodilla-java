eb8b15890653e5d5986e95c0a8fb5f5591d1ff53
Patrycja05 <patrycja.calka@wp.pl>
2020-04-09 22:38:07 +0200

    Test 6.3

---
+++ kodilla-testing/src/test/java/com/kodilla/testing/collection/CollectionTestSuite.java
@@ -0,0 +1,59 @@
+package com.kodilla.testing.collection;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import com.kodilla.testing.collection.OddNumbersExterminator;
+
+import java.util.ArrayList;
+
+public class CollectionTestSuite {
+
+    @Before
+    public void before() {
+        System.out.println("Test begin");
+    }
+
+    @After
+    public void after() {
+        System.out.println("Test end");
+    }
+
+    @Test
+    public void testOddNumbersExterminatorEmptyList() {
+        //Given
+        OddNumbersExterminator exterminator = new OddNumbersExterminator();
+        ArrayList<Integer> emptyList = new ArrayList<>();
+
+        //When
+        ArrayList<Integer> result = exterminator.exterminate(emptyList);
+        System.out.println("List contains: " + result);
+
+        //Then
+        Assert.assertEquals(0, result.size());
+    }
+
+    @Test
+    public void testOddNumbersExterminatorNormalList() {
+
+        //Given
+        OddNumbersExterminator exterminator = new OddNumbersExterminator();
+        ArrayList<Integer> numbers = new ArrayList<>();
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+        numbers.add(4);
+
+        ArrayList<Integer> evenList = new ArrayList<>();
+        evenList.add(2);
+        evenList.add(4);
+
+        //When
+        ArrayList<Integer> result1 = exterminator.exterminate(numbers);
+        System.out.println("List contains: " + result1);
+
+        //Then
+        Assert.assertEquals(2, result1.size());
+    }
+}
