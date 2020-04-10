f5b6a174c7ab78dc7a1c37cb1c462e9bc545eb36
Patrycja05 <patrycja.calka@wp.pl>
2020-04-10 12:06:48 +0200

    Test 6.3

--- kodilla-testing/src/main/java/com/kodilla/testing/collection/OddNumbersExterminator.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/collection/OddNumbersExterminator.java
@@ -1,4 +1,19 @@
 package com.kodilla.testing.collection;
 
+import java.util.ArrayList;
+
 public class OddNumbersExterminator {
+
+    public ArrayList<Integer> numbers;
+    public ArrayList<Integer> numbers1;
+
+    public ArrayList<Integer> exterminate(ArrayList<Integer> numbers) {
+        ArrayList<Integer> numbers1 = new ArrayList<Integer>();
+        for (Integer number: numbers) {
+            if (number % 2 == 0) {
+                numbers1.add(number);
+            }
+        }
+        return numbers1;
+    }
 }
