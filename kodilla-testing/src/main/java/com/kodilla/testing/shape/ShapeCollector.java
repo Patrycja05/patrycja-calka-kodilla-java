bee21457a94048012018d67d73e223770d05b20b
Patrycja05 <patrycja.calka@wp.pl>
2020-04-13 12:39:52 +0200

    Shapes.v2

--- kodilla-testing/src/main/java/com/kodilla/testing/shape/ShapeCollector.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/shape/ShapeCollector.java
@@ -6,14 +6,12 @@ import java.util.ArrayList;
 
 public class ShapeCollector {
     private ArrayList<Shape> shapes = new ArrayList<>();
-    private Shape shape;
-    private int figureNumber;
-    private Shape figures = null;
 
     public int shapesQuantity(){
         return shapes.size();
     }
 
+    Shape shape = null;
     public void addFigure(Shape shape){
         shapes.add(this.shape);
     }
@@ -21,8 +19,8 @@ public class ShapeCollector {
     public void removeFigure(Shape shape){
         shapes.remove(this.shape);
     }
-
-
+    Shape figures = null;
+    int figureNumber = 0;
     public Shape getFigure(int figureNumber){
         if (figureNumber >= 0 && figureNumber < shapes.size()) {
             figures = shapes.get(figureNumber);
