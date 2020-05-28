acd0cbf82d22e629223a1261d8d971027b7f6ba4
Patrycja05 <patrycja.calka@wp.pl>
2020-05-28 12:20:19 +0200

    MovieSTore.v2

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/MovieStore.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/MovieStore.java
@@ -5,7 +5,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class MovieStore {
     public static Map<String, List<String>> getMovies() {
