7965644e45a17a42befa59aec4845a6160893cea
Patrycja05 <patrycja.calka@wp.pl>
2020-10-31 10:59:51 +0100

    zadanie 31.2

--- kodilla-testing2/src/main/java/com/kodilla/testing2/google/GoogleTestingApp.java
+++ kodilla-testing2/src/main/java/com/kodilla/testing2/google/GoogleTestingApp.java
@@ -7,12 +7,21 @@ import org.openqa.selenium.WebElement;
 
 public class GoogleTestingApp {
     public static final String SEARCHFIELD = "gLFyf gsfi";
+    public static final String COOKIE_POLICY = "//*[@id=\"introAgreeButton\"]/div[2]";
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         WebDriver driver = WebDriverConfig.getDriver(WebDriverConfig.CHROME);
         driver.get("https://www.google.com");
 
+        Thread.sleep(4000);
+
+        WebElement cookiePolicy = driver.findElement(By.xpath(COOKIE_POLICY));
+        cookiePolicy.click();
+
+        Thread.sleep(2000);
+
         WebElement searchField = driver.findElement(By.className(SEARCHFIELD));
         searchField.sendKeys("Kodilla");
+        searchField.submit();
     }
 }
