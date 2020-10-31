ae6c4212843e6fd78455f6380c18293c86b377bd
Patrycja05 <patrycja.calka@wp.pl>
2020-10-31 10:19:56 +0100

    Zadanie 31.3

--- kodilla-testing2/src/main/java/com/kodilla/testing2/facebook/FacebookTestingApp.java
+++ kodilla-testing2/src/main/java/com/kodilla/testing2/facebook/FacebookTestingApp.java
@@ -6,16 +6,14 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
 
-public class FacebookTestingApp {
+public class FacebookTestingApp  {
     public static final String XPATH_DATA_POLICY = "//div[contains(@class, \"_9fiw\")]/button[2]";
     public static final String XPATH_CREAT_ACCOUNT = "//div[contains(@class, \"_6ltg\")]/a";
     public static final String XPATH_SELECT_DAY = "//div[contains(@class, \"_5k_5\")]/span/span/select[1]";
     public static final String XPATH_SELECT_MONTH = "//div[contains(@class, \"_5k_5\")]/span/span/select[2]";
     public static final String XPATH_SELECT_YEAR = "//div[contains(@class, \"_5k_5\")]/span/span/select[3]";
-    public static final String XPATH_FIRST_NAME = "//*[@id=\"u_1_b\"]";
 
-
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         WebDriver driver = WebDriverConfig.getDriver(WebDriverConfig.CHROME);
         driver.get("https://www.facebook.com/");
 
@@ -25,31 +23,18 @@ public class FacebookTestingApp {
         WebElement createAccount = driver.findElement(By.xpath(XPATH_CREAT_ACCOUNT));
         createAccount.click();
 
-        //while (!driver.findElement(By.xpath(XPATH_CREAT_ACCOUNT)).isDisplayed());
-        while (!driver.findElement(By.xpath("//select[1]")).isDisplayed());
-
-        WebElement firstName = driver.findElement(By.xpath(XPATH_FIRST_NAME));
-        firstName.sendKeys("Firstname");
-
-        WebElement lastName = driver.findElement(By.xpath("//*[@id=\"u_2_d\"]"));
-        lastName.sendKeys("Lastname");
-
-        WebElement phoneNumber = driver.findElement(By.xpath("//*[@id=\"u_2_g\"]"));
-        phoneNumber.sendKeys("111222333");
-
-        WebElement password = driver.findElement(By.xpath("//*[@id=\"password_step_input\"]"));
-        password.sendKeys("Testowehaslo1.");
+        Thread.sleep(4000);
 
         WebElement selectDay = driver.findElement(By.xpath(XPATH_SELECT_DAY));
         Select select1 = new Select(selectDay);
-        select1.selectByValue("29");
+        select1.selectByIndex(2);
 
         WebElement selectMonth = driver.findElement(By.xpath(XPATH_SELECT_MONTH));
         Select select2 = new Select(selectMonth);
-        select2.selectByValue("wrz");
+        select2.selectByIndex(4);
 
         WebElement selectYear = driver.findElement(By.xpath(XPATH_SELECT_YEAR));
         Select select3 = new Select(selectYear);
-        select3.selectByValue("2019");
+        select3.selectByIndex(22);
     }
 }
